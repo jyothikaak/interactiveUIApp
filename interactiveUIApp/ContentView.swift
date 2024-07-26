8d75fffa0991b089c772f6272eb88a7985527631
Jyothika <ak>
2024-07-26 11:45:33 -0400

    Initial Commit

---
+++ interactiveUIApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  interactiveUIApp
+//
+//  Created by Jyothika Akkineni on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(textTitle)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name"){
+                textTitle = "Welcome \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ interactiveUIApp/interactiveUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIAppApp.swift
+//  interactiveUIApp
+//
+//  Created by Jyothika Akkineni on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
